c0eec2a95c919621777a0c9911d4a0a90c82c42a
mairabassetto <mairabassetto@gmail.com>
2022-02-18 17:44:13 -0300

    Automatic logout

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosError } from 'axios';
 import { parseCookies, setCookie } from 'nookies';
+import { signOut } from '../contexts/AuthContext';
 
 let cookies = parseCookies();
 let isRefreshing = false;
@@ -64,8 +65,10 @@ api.interceptors.response.use(response => {
           }   
         })
       })
-    } else {
-      //deslogar o usuário
+    } else { //deslogar o usuário
+      signOut();
     }
   }
+
+  return Promise.reject(error);
 })
